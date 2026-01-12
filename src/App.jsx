9eea267b60459384020ea29b2381dde97f2968d4
Belmouk <el.mehdi.boukhiam@gmail.com>
2026-01-12 15:15:09 +0100

    chore: intial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <header>
+        <h1>Pokemon Memory Game!</h1>
+      </header>
+      <main></main>
+    </>
+  );
+}
+
+export default App;
